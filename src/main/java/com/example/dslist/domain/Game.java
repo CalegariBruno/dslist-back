18dd3a9ce7fa2c743137e8ce556f9d1b92afd065
CalegariBruno <brunocalegari7@gmail.com>
2025-01-20 16:07:55 -0300

    feat: entidade game e insercao com sql

---
+++ src/main/java/com/example/dslist/domain/Game.java
@@ -0,0 +1,35 @@
+package com.example.dslist.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tb_game")
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Data
+public class Game {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String platforms;
+    private Double score;
+    private String genre;
+
+    @Column(name = "game_year")
+    private Integer year;
+    private String imgUrl;
+
+    @Column(name = "short_description", columnDefinition = "TEXT")
+    private String shortDescription;
+
+    @Column(name = "long_description", columnDefinition = "TEXT")
+    private String longDescription;
+
+}
